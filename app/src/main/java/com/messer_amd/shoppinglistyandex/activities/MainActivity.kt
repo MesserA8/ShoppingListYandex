49e68000f916950bdf3a0165f4d85fa8bef3f0f2
MesserA8 <atihon09@gmail.com>
2023-04-07 13:14:14 +0300

    Base fragment and fragment manager added

--- app/src/main/java/com/messer_amd/shoppinglistyandex/activities/MainActivity.kt
+++ app/src/main/java/com/messer_amd/shoppinglistyandex/activities/MainActivity.kt
@@ -1,7 +1,9 @@
-package com.messer_amd.shoppinglistyandex
+package com.messer_amd.shoppinglistyandex.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.messer_amd.shoppinglistyandex.R
 import com.messer_amd.shoppinglistyandex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,10 +13,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setBottomNavListener()
     }
 
     private fun setBottomNavListener(){
        binding.bNav.setOnItemSelectedListener {
+           when(it.itemId){
+               R.id.settings -> {
+                   Log.d("MyLog", "Settings")
+               }
+               R.id.shop_list -> {
+                   Log.d("MyLog", "List")
+               }
+               R.id.new_item -> {
+                   Log.d("MyLog", "New")
+               }
+           }
            true
        }
     }
